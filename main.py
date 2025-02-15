55174dc81afc170a048e3c57fcc59286ddbdd554
Сергей Карсканов <kapckahob97@gmail.com>
2025-02-15 19:15:58 +0300

    add func mask_account_card

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card
 
 
 if __name__ == "__main__":
-    mask_number = get_mask_card_number("8888745612345879")
-    mask_account = get_mask_account("15616565164884894984")
-    print(mask_number)
-    print(mask_account)
+    # mask_number = get_mask_card_number("8888745612345879")
+    # mask_account = get_mask_account("15616565164884894984")
+    # print(mask_number)
+    # print(mask_account)
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
---
+++ src/widget.py
@@ -0,0 +1,34 @@
+def mask_account_card(info_from_the_client: str) -> str:
+    """ Маскировка номера карты или счета """
+
+    # Извлечение номера карты/счета из строки
+    number_card_or_check = ""
+    name_operations = ''
+
+    # Получение номера карты/счета из данных пользователя
+    for number in info_from_the_client:
+        if number.isdigit():
+            number_card_or_check += number
+        else:
+            name_operations += number
+    name_operations = name_operations.replace(" ", "")
+
+    # Выбор способа маскировки (счет/карта)
+    if name_operations.lower() == "Счет".lower():
+        is_check = True
+    else:
+        is_check = False
+
+    # Маскировка номера счета
+    if is_check:
+        if len(number_card_or_check) < 6:
+            return "Введите корректный номер счета!"
+        else:
+            return f"**{number_card_or_check[-4:]}"
+
+    else:
+        # Маскировка номера карты
+        if len(number_card_or_check) != 16:
+            return "Неверный номер карты!"
+        else:
+            return f"{number_card_or_check[:4]} {number_card_or_check[4:6]}** **** {number_card_or_check[-4:]}"
