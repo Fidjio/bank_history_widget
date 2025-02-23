7f95cd0bfb17a69250848c22ee9f488a601a5080
Сергей Карсканов <kapckahob97@gmail.com>
2025-02-24 00:53:34 +0300

    add filter_by_state func in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,10 @@
-def filter_by_state():
-    pass
+def filter_by_state(list_of_operation_dictionaries: list[dict], state: str ='EXECUTED') -> list[dict]:
+    """ Принимает словарь с данными об операции и значение ключа state.
+    Возвращает инфо об операции в зависимости от выбранного ключа """
+    new_list_user_operations = list()
+
+    for user_operation in list_of_operation_dictionaries:
+        if user_operation['state'] == state:
+            new_list_user_operations.append(user_operation)
+
+    return new_list_user_operations
