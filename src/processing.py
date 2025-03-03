4a704a6f3358dbca855c7fe2b6e4daf2ccf84f0c
Сергей Карсканов <kapckahob97@gmail.com>
2025-03-04 00:00:16 +0300

    fix func filter_by_state in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(list_of_operation_dictionaries: list[dict], state: str = "EXECUTED") -> list[dict]:
+from isort.io import Empty
+
+
+def filter_by_state(list_of_operation_dictionaries: list[dict], state: str = "EXECUTED") -> list[dict] | str:
     """Принимает словарь с данными об операции и значение ключа state.
     Возвращает инфо об операции в зависимости от выбранного ключа"""
     new_list_user_operations = list()
@@ -6,6 +9,8 @@ def filter_by_state(list_of_operation_dictionaries: list[dict], state: str = "EX
     for user_operation in list_of_operation_dictionaries:
         if user_operation["state"] == state:
             new_list_user_operations.append(user_operation)
+    if not new_list_user_operations:
+        return "Значений с заданным ключом нет в списке словарей"
 
     return new_list_user_operations
 
