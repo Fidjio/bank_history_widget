f26218f72c109b5b12f74699139204947c848656
Сергей Карсканов <kapckahob97@gmail.com>
2025-03-13 01:23:21 +0300

    add func card_number_generator in generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Iterator, Optional
+from typing import Any, Dict, Iterator, Optional, Generator, LiteralString
 
 
 def filter_by_currency(transactions: list[dict[str, Any]], currency: str) -> Iterator[Dict[str, Any]]:
@@ -12,6 +12,7 @@ def filter_by_currency(transactions: list[dict[str, Any]], currency: str) -> Ite
     except KeyError:
         pass
 
+
 def transaction_descriptions(transactions: list[dict[str, Any]]) -> Iterator[Optional[str]]:
     """Принимает список словарей с транзакциями и
     возвращает описание каждой операции по очереди"""
@@ -20,3 +21,20 @@ def transaction_descriptions(transactions: list[dict[str, Any]]) -> Iterator[Opt
             yield dict_.get("description")
     except Exception:
         pass
+
+
+def card_number_generator(start_num: int, stop_num: int) -> Generator[LiteralString, Any, str | None]:
+    """выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где
+    X — цифра номера карты. Генератор может сгенерировать номера карт в заданном диапазоне
+    от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Генератор должен принимать начальное и конечное значения для генерации диапазона номеров."""
+    if stop_num >= 9999999999999999:
+        return "Вы вышли за рамки диапазона"
+    number_card = "0000000000000000"
+    for i in range(start_num, stop_num):
+        number_card = number_card[:-len(str(i))] + str(i)
+        # Результат выводится с помощью генератора списков
+        num_card_result = " ".join([number_card[i:i + 4] for i in range(0, len(number_card), 4)])
+        yield num_card_result
+
+    return None
