7f46dc03f6809679bdd96b5adb76d912e17352b1
Сергей Карсканов <kapckahob97@gmail.com>
2025-03-13 00:17:49 +0300

    add func transactions_descriptions in generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Iterator
+from typing import Any, Dict, Iterator, Optional
 
 
 def filter_by_currency(transactions: list[dict[str, Any]], currency: str) -> Iterator[Dict[str, Any]]:
@@ -10,4 +10,13 @@ def filter_by_currency(transactions: list[dict[str, Any]], currency: str) -> Ite
             if dict_["operationAmount"]["currency"]["code"] == currency:
                 yield dict_
     except KeyError:
+        pass
+
+def transaction_descriptions(transactions: list[dict[str, Any]]) -> Iterator[Optional[str]]:
+    """Принимает список словарей с транзакциями и
+    возвращает описание каждой операции по очереди"""
+    try:
+        for dict_ in transactions:
+            yield dict_.get("description")
+    except Exception:
         pass
