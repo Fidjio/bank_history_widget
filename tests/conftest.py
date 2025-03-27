37476397e25866d08ff5299e62e6c0d8fdbccde5
Сергей Карсканов <kapckahob97@gmail.com>
2025-03-28 01:35:02 +0300

    pytest --cov=src --cov-report=html

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-import pandas as pd
 
 
 @pytest.fixture
@@ -232,21 +231,16 @@ def transaction_test_value_200():
 
 
 @pytest.fixture
-def info_transactions_csv():
-    df = pd.DataFrame({
-        'Bob': ['Мне это понравилось.', 'Это было ужасно.'],
-        'Sue': ['Довольно хорошо.', 'Без вкуса.']
-    })
-
-
-@pytest.fixture
 def info_to_test_read_files():
-    df = [{'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210', 'currency_name': 'Sol',
-      'currency_code': 'PEN', 'from': 'Счет 58803664561298323391', 'to': 'Счет 39745660563456619397',
-      'description': 'Перевод организации'},
-     {'id': '3598919', 'state': 'CANCELED', 'date': '2020-12-06T23:00:58Z', 'amount': '29740', 'currency_name': 'Peso',
-      'currency_code': 'COP', 'from': 'Discover 3172601889670065', 'to': 'Discover 0720428384694643',
-      'description': 'Перевод с карты на карту'}]
+    df = [
+        {'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': '16210',
+        'currency_name': 'Sol', 'currency_code': 'PEN', 'from': 'Счет 58803664561298323391',
+        'to': 'Счет 39745660563456619397', 'description': 'Перевод организации'},
+        {'id': '3598919', 'state': 'CANCELED', 'date': '2020-12-06T23:00:58Z',
+         'amount': '29740', 'currency_name': 'Peso', 'currency_code': 'COP',
+         'from': 'Discover 3172601889670065', 'to': 'Discover 0720428384694643',
+        'description': 'Перевод с карты на карту'}
+          ]
 
     return df
 
